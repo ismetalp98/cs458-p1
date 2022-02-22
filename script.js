eb8e93340a26409ada34960992c4e3899044c8e7
İsmet Alp Eren <alp.eren@ug.bilkent.edu.tr>
2022-02-23 00:37:52 +0300

    feat(frontend): scripts

--- script.js
+++ script.js
@@ -1,41 +1,52 @@
-let inputTouched = {
-    email: false,
-    password: false
+import users from "../frontend/users";
+
+const email = document.getElementById("inputEmail")
+const password = document.getElementById("inputPassword")
+const emailWarningEmp = document.getElementById("warningEmailEmp")
+const emailWarningEmail = document.getElementById("warningEmailEmail")
+const emailWarningPhone = document.getElementById("warningEmailPhone")
+const passwordWarning = document.getElementById("warningPassword")
+const loginButton = document.getElementById("signin-button").onclick = function login() {
+    console.log(usersData.users);
 }
 
-const inputEmail = document.getElementById("inputEmail")
-const inputPassword = document.getElementById("inputPassword")
-const inputWrapperEmail = document.getElementById("input-wrapper-email")
-const inputWrapperPassword = document.getElementById("input-wrapper-password")
-const warningEmail = document.getElementById("warningEmail")
-const warningPassword = document.getElementById("warningPassword")
-
-const inputOnBlur = (ev) =>{
-    if(inputTouched.email){
-        if(!validateEmail(inputEmail.value) && !validatePhone(inputEmail.value)){
-            warningEmail.style.display="block"
-            inputEmail.style.borderBottom='2px solid #e87c03'
+const onBlur = (ev) => {
+    if (ev.name === "email") {
+        if (ev.value === "") {
+            emailWarningEmail.style.display = "none";
+            emailWarningPhone.style.display = "none";
+            emailWarningEmp.style.display = "block";
+            email.style.borderBottom = '2px solid #e87c03';
         }
-        else{
-            warningEmail.style.display="none"
-            inputEmail.style.borderBottom="none"
+        else if (isNaN(parseInt(ev.value)) && !validateEmail(ev.value)) {
+            emailWarningEmail.style.display = "block";
+            emailWarningEmp.style.display = "none";
+            emailWarningPhone.style.display = "none";
+            email.style.borderBottom = '2px solid #e87c03';
+        }
+        else if (!isNaN(parseInt(ev.value)) && !validatePhone(ev.value)) {
+            emailWarningEmail.style.display = "none";
+            emailWarningEmp.style.display = "none";
+            emailWarningPhone.style.display = "block";
+            email.style.borderBottom = '2px solid #e87c03';
+        } else {
+            emailWarningEmail.style.display = "none";
+            emailWarningEmp.style.display = "none";
+            emailWarningPhone.style.display = "none";
+            email.style.borderBottom = 'none';
         }
     }
-    if(inputTouched.password){
-        if(!(inputPassword.value.length >= 4 && inputPassword.value.length <= 60)){
-            warningPassword.style.display="block"
-            inputPassword.style.borderBottom='2px solid #e87c03'
+    if (ev.name === "password") {
+        if (String(ev.value).length < 4 || String(ev.value).length > 60) {
+            passwordWarning.style.display = "block";
+            password.style.borderBottom = '2px solid #e87c03';
         }
-        else{
-            warningPassword.style.display="none"
-            inputPassword.style.borderBottom="none"
+        else {
+            passwordWarning.style.display = "none";
+            password.style.borderBottom = 'none';
         }
     }
-}
-
-const inputOnFocus = (ev) =>{
-    inputTouched[ev.name] = true;
-}
+};
 
 const validateEmail = email => {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
---
+++ users.js
@@ -0,0 +1,12 @@
+export const users = {
+    "users": [
+        {
+            "username": "05362284637",
+            "password": "Ali3457y"
+        },
+        {
+            "username": "ismet@gmail.com",
+            "password": "asd123er"
+        }
+    ]
+}
