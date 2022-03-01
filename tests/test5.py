7c124827380a9c07f1168fe90f614ed360b32763
isikosoy <isik.ozsoy@ug.bilkent.edu.tr>
2022-03-01 22:39:25 +0300

    unnecessary imports were removed.

--- tests/test5.py
+++ tests/test5.py
@@ -1,12 +1,7 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver import ActionChains
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 
-import time
 from decouple import config
 
 
