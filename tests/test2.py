1251627f14426e0740f56916f63acd0321265c96
denizhankemeroz <denizhan.kemeroz@ug.bilkent.edu.tr>
2022-03-01 22:29:42 +0300

    test 2.5 is deleted

--- tests/test2.py
+++ tests/test2.py
@@ -63,35 +63,5 @@ def test2():
     url2 = driver.current_url
     assert url1 == url2
 
-    # Test 2.5 After login with facebook go back and forward pages
-
-    driver.find_element(By.XPATH, '/html/body/div[2]/div/div[5]').click()
-    time.sleep(3)
-
-    main_window = driver.current_window_handle
-    for fb_login in driver.window_handles:
-        if fb_login != main_window:  # Facebook login page
-            driver.switch_to.window(fb_login)
-            break
-
-    driver.find_element(By.ID, "email").send_keys("alp@gmail.com")
-    time.sleep(1)
-    driver.find_element(By.ID, "pass").send_keys("fds135bo")
-    time.sleep(1)
-    driver.find_element(By.ID, "loginbutton").click()
-    time.sleep(1)
-
-    driver.switch_to.window(main_window)
-    current_url = driver.current_url
-    expected_url = "https://ismetalp98.github.io/cs458-p1/emptyPage.html?logged-in"
-
-    assert current_url == expected_url
-
-    url1 = current_url
-    driver.get("https://ismetalp98.github.io/cs458-p1/")
-    time.sleep(1)
-    url2 = driver.current_url
-    assert url1 == url2
-
 
 test2()
