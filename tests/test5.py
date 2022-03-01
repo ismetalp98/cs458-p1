bfca237408497b81ad7580abd2573d899f7e039c
Eylül Çağlar <caglar.eylul99@gmail.com>
2022-03-01 12:38:57 +0300

    Fix in test 5

--- tests/test5.py
+++ tests/test5.py
@@ -4,6 +4,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 
 import time
 from decouple import config
