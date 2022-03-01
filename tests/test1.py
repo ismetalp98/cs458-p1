7abc555f41e6a570ca3863abe3322456a829e3d1
isikosoy <isik.ozsoy@ug.bilkent.edu.tr>
2022-03-01 22:41:33 +0300

    unnecessary imports were removed.

--- tests/test1.py
+++ tests/test1.py
@@ -1,8 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver import ActionChains
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 import time
